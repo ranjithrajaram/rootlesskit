0ee7746352dd2cfb6eb9592682633065adbe235a
Ranjith Rajaram <ranjith@redhat.com>
2026-06-18 19:10:06 +0530

    port/tcp: bypass splice to prevent deadlocks under slow client
    Bypass Go's optimized zero-copy splice path by wrapping the
    destination connection in a writerOnly struct. This forces
    standard user-space copying (read/write loop) which is fully
    immune to kernel-level/pipe-level event starvation deadlocks
    that occur under zero-window scenarios with slow clients.
    
    Signed-off-by: Ranjith Rajaram <ranjith@redhat.com>

--- pkg/port/builtin/parent/tcp/tcp.go
+++ pkg/port/builtin/parent/tcp/tcp.go
@@ -1,20 +1,15 @@
 package tcp
 
 import (
-	"errors"
 	"fmt"
 	"io"
 	"net"
 	"os"
-	"runtime"
 	"strconv"
 	"sync"
-	"syscall"
 
 	"github.com/rootless-containers/rootlesskit/v2/pkg/port"
 	"github.com/rootless-containers/rootlesskit/v2/pkg/port/builtin/msg"
-	"github.com/sirupsen/logrus"
-	"golang.org/x/sys/unix"
 )
 
 func Run(socketPath string, spec port.Spec, stopCh <-chan struct{}, stoppedCh chan error, logWriter io.Writer) error {
@@ -79,248 +74,29 @@ func copyConnToChild(c net.Conn, socketPath string, spec port.Spec, stopCh <-cha
 	return nil
 }
 
-var errQuit = errors.New("quit")
-
-const maxSpliceSize = 65536
-
-func spliceCopy(rawDst, rawSrc syscall.RawConn, quit <-chan struct{}) error {
-	var pipeFds [2]int
-	if err := unix.Pipe2(pipeFds[:], unix.O_CLOEXEC|unix.O_NONBLOCK); err != nil {
-		return err
-	}
-	pipeRead := pipeFds[0]
-	pipeWrite := pipeFds[1]
-	defer unix.Close(pipeRead)
-	defer unix.Close(pipeWrite)
-
-	for {
-		select {
-		case <-quit:
-			return nil
-		default:
-		}
-
-		var n int
-		var err error
-		readErr := rawSrc.Read(func(fd uintptr) bool {
-			var sn int64
-			sn, err = unix.Splice(int(fd), nil, pipeWrite, nil, maxSpliceSize, unix.SPLICE_F_NONBLOCK|unix.SPLICE_F_MOVE)
-			n = int(sn)
-			if err == unix.EAGAIN || err == unix.EWOULDBLOCK {
-				select {
-				case <-quit:
-					err = errQuit
-					return true
-				default:
-				}
-				return false
-			}
-			return true
-		})
-
-		if readErr != nil {
-			return readErr
-		}
-		if err != nil {
-			if err == errQuit {
-				return nil
-			}
-			if err == unix.EINTR {
-				continue
-			}
-			return err
-		}
-		if n == 0 {
-			return nil
-		}
-
-		inPipe := n
-		for inPipe > 0 {
-			var m int
-			writeErr := rawDst.Write(func(fd uintptr) bool {
-				var sm int64
-				sm, err = unix.Splice(pipeRead, nil, int(fd), nil, inPipe, unix.SPLICE_F_NONBLOCK|unix.SPLICE_F_MOVE)
-				m = int(sm)
-				if err == unix.EAGAIN || err == unix.EWOULDBLOCK {
-					select {
-					case <-quit:
-						err = errQuit
-						return true
-					default:
-					}
-					return false
-				}
-				return true
-			})
-
-			if writeErr != nil {
-				return writeErr
-			}
-			if err != nil {
-				if err == errQuit {
-					return nil
-				}
-				if err == unix.EINTR {
-					continue
-				}
-				return err
-			}
-			if m == 0 {
-				return io.ErrUnexpectedEOF
-			}
-			inPipe -= m
-		}
-	}
-}
-
-var (
-	spliceSupported bool
-	probeOnce       sync.Once
-)
-
-// probeSplice checks if the splice(2) system call is supported between socket and pipe and not blocked by seccomp
-func probeSplice() bool {
-	probeOnce.Do(func() {
-		// Create a socket pair to test socket -> pipe splicing
-		fds, err := syscall.Socketpair(syscall.AF_UNIX, syscall.SOCK_STREAM, 0)
-		if err != nil {
-			logrus.Debugf("splice support probe failed to create socketpair: %v (splice will be disabled)", err)
-			return
-		}
-		defer syscall.Close(fds[0])
-		defer syscall.Close(fds[1])
-
-		var pipeFds [2]int
-		if err := unix.Pipe2(pipeFds[:], unix.O_CLOEXEC|unix.O_NONBLOCK); err != nil {
-			logrus.Debugf("splice support probe failed to create pipe: %v (splice will be disabled)", err)
-			return
-		}
-		defer unix.Close(pipeFds[0])
-		defer unix.Close(pipeFds[1])
-
-		_, err = unix.Splice(fds[0], nil, pipeFds[1], nil, 0, unix.SPLICE_F_NONBLOCK)
-		if err != nil {
-			if err != unix.EAGAIN && err != unix.EWOULDBLOCK && err != unix.EINTR {
-				logrus.Debugf("splice support probe failed: %v (splice will be disabled)", err)
-				return
-			}
-		}
-		spliceSupported = true
-	})
-	return spliceSupported
+// writerOnly hides the ReadFrom/splice interface to force user-space read/write copy
+type writerOnly struct {
+	io.Writer
 }
 
 // bicopy is based on libnetwork/cmd/proxy/tcp_proxy.go .
 // NOTE: sendfile(2) cannot be used for sockets
 func bicopy(x, y net.Conn, quit <-chan struct{}) {
-	// Guard lifetime of net.Conn objects so they are not finalized/closed by GC while splicing
-	defer runtime.KeepAlive(x)
-	defer runtime.KeepAlive(y)
-
-	sconnX, okX := x.(interface {
-		SyscallConn() (syscall.RawConn, error)
-	})
-	sconnY, okY := y.(interface {
-		SyscallConn() (syscall.RawConn, error)
-	})
-
-	// Fallback to standard io.Copy if we cannot get raw fds or splice is unavailable/blocked
-	if !okX || !okY || !probeSplice() {
-		var wg sync.WaitGroup
-		var broker = func(to, from net.Conn) {
-			io.Copy(to, from)
-			if fromTCP, ok := from.(*net.TCPConn); ok {
-				fromTCP.CloseRead()
-			}
-			if toTCP, ok := to.(*net.TCPConn); ok {
-				toTCP.CloseWrite()
-			}
-			wg.Done()
-		}
-
-		wg.Add(2)
-		go broker(x, y)
-		go broker(y, x)
-		finish := make(chan struct{})
-		go func() {
-			wg.Wait()
-			close(finish)
-		}()
-
-		select {
-		case <-quit:
-		case <-finish:
-		}
-		x.Close()
-		y.Close()
-		<-finish
-		return
-	}
-
-	rawX, errX := sconnX.SyscallConn()
-	rawY, errY := sconnY.SyscallConn()
-	if errX != nil || errY != nil {
-		var wg sync.WaitGroup
-		var broker = func(to, from net.Conn) {
-			io.Copy(to, from)
-			if fromTCP, ok := from.(*net.TCPConn); ok {
-				fromTCP.CloseRead()
-			}
-			if toTCP, ok := to.(*net.TCPConn); ok {
-				toTCP.CloseWrite()
-			}
-			wg.Done()
-		}
-
-		wg.Add(2)
-		go broker(x, y)
-		go broker(y, x)
-		finish := make(chan struct{})
-		go func() {
-			wg.Wait()
-			close(finish)
-		}()
-
-		select {
-		case <-quit:
-		case <-finish:
-		}
-		x.Close()
-		y.Close()
-		<-finish
-		return
-	}
-
-	// We use the raw splice event loop
 	var wg sync.WaitGroup
+	var broker = func(to, from net.Conn) {
+		io.Copy(writerOnly{to}, from)
+		if fromTCP, ok := from.(*net.TCPConn); ok {
+			fromTCP.CloseRead()
+		}
+		if toTCP, ok := to.(*net.TCPConn); ok {
+			toTCP.CloseWrite()
+		}
+		wg.Done()
+	}
+
 	wg.Add(2)
-
-	go func() {
-		defer wg.Done()
-		if err := spliceCopy(rawY, rawX, quit); err != nil {
-			logrus.Debugf("spliceCopy x->y error: %v", err)
-		}
-		if yTCP, ok := y.(*net.TCPConn); ok {
-			yTCP.CloseWrite()
-		}
-		if xTCP, ok := x.(*net.TCPConn); ok {
-			xTCP.CloseRead()
-		}
-	}()
-
-	go func() {
-		defer wg.Done()
-		if err := spliceCopy(rawX, rawY, quit); err != nil {
-			logrus.Debugf("spliceCopy y->x error: %v", err)
-		}
-		if xTCP, ok := x.(*net.TCPConn); ok {
-			xTCP.CloseWrite()
-		}
-		if yTCP, ok := y.(*net.TCPConn); ok {
-			yTCP.CloseRead()
-		}
-	}()
-
+	go broker(x, y)
+	go broker(y, x)
 	finish := make(chan struct{})
 	go func() {
 		wg.Wait()
